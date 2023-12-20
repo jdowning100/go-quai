94181853b3d0e6708745cb8aff2a8a317da041c9
Alan Orwick <alan.kev11@gmail.com>
2023-12-20 09:56:10 -0600

    remove tx script

--- core/txscript/script.go
+++
@@ -1,8 +0,0 @@
-package txscript
-
-// These are the constants specified for maximums in individual scripts.
-const (
-	MaxOpsPerScript       = 201 // Max number of non-push operations.
-	MaxPubKeysPerMultiSig = 20  // Multisig can't have more sigs than this.
-	MaxScriptElementSize  = 520 // Max bytes pushable to the stack.
-)

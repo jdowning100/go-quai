8e95f99a69fd8f4fdc9e14e8b4f6d388b36c722b
gop <gameofpointers@proton.me>
2024-01-31 14:45:21 -0600

    Bootnodes for testing

--- common/bootnodes.go
+++ common/bootnodes.go
@@ -2,9 +2,8 @@ package common
 
 var (
 	BootstrapPeers = []string{
-		"/ip4/146.148.66.22/tcp/4001/p2p/12D3KooWRQrLVEeJtfyKoJDYWYjryBKR8qxkDooMMzyf2ZpLaZRR",
-		"/ip4/35.190.147.237/tcp/4001/p2p/12D3KooWSb49ccXFWPCsvi7rzCbqBUK2xfuRC2xbo6KnUZk3YaVg",
-		"/ip4/35.194.7.78/tcp/4001/p2p/12D3KooWR3xMB6sCpsowQcvtdMKmKbTaiDcDFAXuWABdZVPWaVuo",
-		"/ip4/34.136.140.151/tcp/4001/p2p/12D3KooWJnWmBukEbZtGPPJvT1r4tQ97CRSGmnjHewcrjNB8oRxU",
+		"/ip4/35.227.43.191/tcp/4001/p2p/12D3KooWFtYckZKyMtqkdjiQD2wMiPTT411ZWKLQhsv6hCUHbeNN",
+		"/ip4/34.30.223.152/tcp/4001/p2p/12D3KooWMqCFqMxswdr44E7Q6e8YGi2ifVv7ujvtpkgLovvSiDgF",
+		"/ip4/34.70.137.228/tcp/4001/p2p/12D3KooWF7q3aXNa1xt74NVMdRTJjrEHL1w3f4gSsWAARFQkFbXi",
 	}
 )

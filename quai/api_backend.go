2013f00087551b9da3d77ca4a669751265467f16
Hussam <hussam@dominantstrategies.io>
2024-04-16 16:27:49 -0500

    Introduce Block and Header views to optimize bandwidth usage across topics

--- quai/api_backend.go
+++ quai/api_backend.go
@@ -574,9 +574,27 @@ func (b *QuaiAPIBackend) SubscribeExpansionEvent(ch chan<- core.ExpansionEvent) 
 	return b.quai.core.SubscribeExpansionEvent(ch)
 }
 
-// ///////////////////////////
-// /////// P2P ///////////////
-// ///////////////////////////
-func (b *QuaiAPIBackend) BroadcastBlock(block *types.WorkObject, location common.Location) error {
-	return b.quai.p2p.Broadcast(location, block)
+//////////////////////////////
+////////// P2P ///////////////
+//////////////////////////////
+func (b *QuaiAPIBackend) BroadcastWorkObject(wo *types.WorkObject, location common.Location) error {
+	errChan := make(chan error, 2)
+
+	go func() {
+		err := b.quai.p2p.Broadcast(location, wo.ConvertToBlockView())
+		errChan <- err
+	}()
+
+	go func() {
+		err := b.quai.p2p.Broadcast(location, wo.ConvertToHeaderView())
+		errChan <- err
+	}()
+
+	// Return the error if any of the broadcasts fail
+	for i := 0; i < 2; i++ {
+		if err := <-errChan; err != nil {
+			return err
+		}
+	}
+	return nil
 }

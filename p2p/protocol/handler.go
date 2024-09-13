7d17d6b415217a19f791e8cd98ee64b34b7ab385
gop <gameofpointers@proton.me>
2024-09-12 19:18:23 -0500

    Changed the return to continue in p2p handler

--- p2p/protocol/handler.go
+++ p2p/protocol/handler.go
@@ -78,7 +78,7 @@ func QuaiProtocolHandler(stream network.Stream, node QuaiP2PNode) {
 
 			log.Global.Errorf("error reading message from stream: %s", err)
 			// TODO: handle error
-			return
+			continue
 		}
 
 		// Send to worker goroutines

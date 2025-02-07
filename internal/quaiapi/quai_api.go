57b74e902010f3e6573437982ed5a95e69a5aae2
Jonathan Downing <jonathan@dominantstrategies.io>
2025-02-07 15:05:12 -0600

    Added non-consensus error and revert reason fields to receipts

--- internal/quaiapi/quai_api.go
+++ internal/quaiapi/quai_api.go
@@ -760,7 +760,7 @@ func (s *PublicBlockChainQuaiAPI) Call(ctx context.Context, args TransactionArgs
 	}
 	// If the result contains a revert reason, try to unpack and return it.
 	if len(result.Revert()) > 0 {
-		return nil, newRevertError(result, s.b.NodeLocation())
+		return nil, core.NewRevertError(result, s.b.NodeLocation())
 	}
 	return result.Return(), result.Err
 }
@@ -1373,7 +1373,14 @@ func (s *PublicBlockChainQuaiAPI) GetTransactionReceipt(ctx context.Context, has
 		signer := types.MakeSigner(s.b.ChainConfig(), bigblock)
 		from, _ := types.Sender(signer, tx)
 		fields["from"] = from.Hex()
-
+	}
+	if s.b.Config().IndexAddressUtxos {
+		if receipt.Error != "" {
+			fields["error"] = receipt.Error
+		}
+		if receipt.RevertReason != "" {
+			fields["revertReason"] = receipt.RevertReason
+		}
 	}
 
 	if tx.Type() == types.ExternalTxType {

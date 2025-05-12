872aed9e3ea6069b8300c07bb35b3bd97ead7244
Jonathan Downing <jdowning@utexas.edu>
2025-05-12 18:20:11 -0500

    Return pending block instead of latest in eth_ namespace and attach pending block hash to receipts

--- core/core.go
+++ core/core.go
@@ -1647,6 +1647,10 @@ func (c *Core) PendingBlock() *types.WorkObject {
 	return c.sl.miner.PendingBlock()
 }
 
+func (c *Core) PendingBlockByHash(blockHash common.Hash) *types.WorkObject {
+	return c.sl.miner.worker.PendingBlockByHash(blockHash)
+}
+
 // PendingBlockAndReceipts returns the currently pending block and corresponding receipts.
 func (c *Core) PendingBlockAndReceipts() (*types.WorkObject, types.Receipts) {
 	return c.sl.miner.PendingBlockAndReceipts()
